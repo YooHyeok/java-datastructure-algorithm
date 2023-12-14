a1840312b0cd0b638f6a67ac8e6c6b29abe98c45
webdevyoo <yjk7454@naver.com>
2023-12-14 16:46:43 +0900

    리스트 출력용 toString 구현 및 주석 추가

--- src/datastructure/list/linkedList/LinkedList.java
+++ src/datastructure/list/linkedList/LinkedList.java
@@ -8,26 +8,47 @@ public class LinkedList {
         head = null;
     }
 
+    /**
+     * Node 삽입 메소드 <br/>
+     * (가장 마지막에 삽입)
+     * @param data
+     */
     public void insertNode(String data) {
-        ListNode newNode = new ListNode(data);
+        ListNode newNode = new ListNode(data); // 새 노드 객체 데이터 추가 및 생성
 
-        if(this.head == null) {
+        if(this.head == null) { // 현재 노드가 비어있다면 단일계층(하위 참조 노드가 null) 신규 노드를 바로 추가
             this.head = newNode;
             return;
         }
 
-        ListNode tempNode = this.head;
+        ListNode tempNode = this.head; /* 현재 노드를 기준으로 마지막 노드까지 탐색하기 위해 임시 노드로 추출 */
+
+        /* 현재 노드의 하위 노드가 비어있을때 까지(마지막노드까지) 탐색*/
         while(tempNode.pointer!= null) {
-            tempNode = tempNode.pointer;
+            tempNode = tempNode.pointer; /* 임시 노드를 다음계층의 노드로 초기화 */
         }
 
-        tempNode.pointer = newNode;
+        tempNode.pointer = newNode;// 마지막 노드의 null을 새 노드 객체로 초기화
     }
 
+    /**
+     * 출력시 배열에 담긴 문자열 형태로 출력하기 위한 toString 메소드 구현
+     * @return
+     */
     @Override
     public String toString() {
-        return "LinkedList{" +
-                "head=" + head +
-                '}';
+        StringBuffer sb = new StringBuffer();
+        ListNode tempNode = this.head;
+        sb.append("[");
+        while(tempNode != null) {
+            sb.append(tempNode.getData());
+            tempNode = tempNode.pointer;
+            if(tempNode != null) {
+                sb.append(", ");
+            }
+        }
+        sb.append("]");
+        return sb.toString();
+//        return "LinkedList{head=" + head +'}';
     }
 }
