643c6439957a318396fe1bb7bd1f1e15de51a331
webdevyoo <yjk7454@naver.com>
2024-09-05 00:26:58 +0900

    인프런 Section02_Array - 03) `가위 바위 보` 해설 풀이

--- src/inflearn/section02_array/Quiz03_RockScissorsPaper.java
+++ src/inflearn/section02_array/Quiz03_RockScissorsPaper.java
@@ -16,6 +16,7 @@ public class Quiz03_RockScissorsPaper {
             b[i]=kb.nextInt();
         }
         for(char x :solution(n, a, b).toCharArray()) System.out.println(x);
+        for(char x :answer(n, a, b).toCharArray()) System.out.println(x);
     }
 
     /**
@@ -44,6 +45,18 @@ public class Quiz03_RockScissorsPaper {
         }
 
         return String.valueOf(sb);
+    }
+
+    private static String answer(int n, int[] a, int[] b) {
+        String answer = "";
+        for (int i = 0; i < n; i++) {
+            if (a[i] == b[i]) answer += "D";
+            else if (a[i] == 1 && b[i] == 3) answer += "A";
+            else if (a[i] == 2 && b[i] == 1) answer += "A";
+            else if (a[i] == 3 && b[i] == 2) answer += "A";
+            else answer += "B";
+        }
+        return answer;
     }
 
 }
