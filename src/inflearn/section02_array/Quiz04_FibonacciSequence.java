cad2c107dc942dd34e544aa5b83d368cd4706d84
webdevyoo <yjk7454@naver.com>
2024-09-06 00:11:03 +0900

    인프런 Section02_Array - 04) `피보나치 수열` 해설 풀이1

--- src/inflearn/section02_array/Quiz04_FibonacciSequence.java
+++ src/inflearn/section02_array/Quiz04_FibonacciSequence.java
@@ -9,6 +9,7 @@ public class Quiz04_FibonacciSequence {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         solution(n);
+        answer1(n);
     }
 
     // 1 1 2 3 5 8 13
@@ -21,6 +22,21 @@ public class Quiz04_FibonacciSequence {
             temp = next;
             next = cur + next;
             cur = temp;
+        }
+    }
+
+    /**
+     * 배열을 사용하지 않고 손코딩하는 경우
+     * @param n
+     */
+    private static void answer1(int n) {
+        int a = 1, b = 1, c;
+        System.out.print(a + " " + b);
+        for (int i = 2; i < n; i++) {
+            c = a + b;
+            System.out.print(" " + c);
+            a = b;
+            b = c;
         }
     }
 
