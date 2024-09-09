7e0dae9551e8139df38ce9057b26b22cd1621626
webdevyoo <yjk7454@naver.com>
2024-09-10 00:12:14 +0900

    인프런 Section02_Array - 07) `점수 계산` 일반 풀이

---
+++ src/inflearn/section02_array/Quiz07_ScoreCalculation.java
@@ -0,0 +1,30 @@
+package inflearn.section02_array;
+
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Quiz07_ScoreCalculation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int length = sc.nextInt();
+        int[] intArr = new int[length];
+        for (int i = 0; i < length; i++) {
+            intArr[i] = sc.nextInt();
+        }
+
+        System.out.println(answer(length, intArr));
+
+    }
+
+    private static int answer(int length, int[] intArr) {
+        int answer = 0;
+        int score = 0;
+        for (int i : intArr) {
+            if(i == 1) score ++;
+            else {score = 0;}
+            answer += score;
+        }
+        return answer;
+    }
+
+}
