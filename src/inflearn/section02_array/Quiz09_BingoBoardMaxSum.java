cb175c9c914b82a8ca837d4ef76bfbead9ad3c0e
webdevyoo <yjk7454@naver.com>
2024-09-12 00:25:35 +0900

    인프런 Section02_Array - 09) `격자판 최대합` 스트림 풀이

--- src/inflearn/section02_array/Quiz09_BingoBoardMaxSum.java
+++ src/inflearn/section02_array/Quiz09_BingoBoardMaxSum.java
@@ -18,6 +18,7 @@ public class Quiz09_BingoBoardMaxSum {
 
         System.out.println(answer(length, intArr));
         System.out.println(solution(length, intArr));
+        System.out.println(stream(length, intArr));
 
     }
 
@@ -75,6 +76,42 @@ public class Quiz09_BingoBoardMaxSum {
         max = Math.max(max, xYSum);
 
         return max;
+    }
+
+    /**
+     * max를 구하는 방법을 Stream으로 처리했다. <br/>
+     * mapToInt를 활용하여 IntStream으로 변환하여 Optional의 max값을 구한다. <br/>
+     * @param length
+     * @param intArr
+     * @return
+     */
+    private static int stream(int length, int[][] intArr) {
+        List<Integer> sums = new ArrayList<>();
+        /* 대각선 누적합, 최대값 */
+        int xXSum = 0, xYSum = 0, max = 0;
+
+        for (int i = 0; i < length; i++) {
+            /* 행, 열 누적합 */
+            int rowSum = 0, colSum = 0;
+            for (int j = 0; j < length; j++) {
+                /* 각 행의 누적합 저장*/
+                rowSum += intArr[i][j];
+                /* 각 열의 누적합 저장*/
+                colSum += intArr[j][i];
+            }
+            /* 행/열 값 최대값 처리 */
+            sums.add(colSum);
+            sums.add(rowSum);
+
+            /* 대각선의 누적 합 저장*/
+            xXSum += intArr[i][i];
+            xYSum += intArr[i][length-1-i];
+        }
+        /* 대각선 의 합 최대값 처리*/
+        sums.add(xXSum);
+        sums.add(xYSum);
+
+        return sums.stream().mapToInt(value->value).max().getAsInt();
     }
 
 }
