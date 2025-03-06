5ca3f874ebe44ed9f80f3b4a1313269d710e129d
webdevyoo <yjk7454@naver.com>
2025-03-07 02:08:59 +0900

    인프런 Section03_투포인터,슬라이딩윈도우 - 05) `연속된 자연수의 합` 해설 풀이1

--- src/inflearn/section03_twoPointer/Quiz05_SeqNumberSum.java
+++ src/inflearn/section03_twoPointer/Quiz05_SeqNumberSum.java
@@ -8,6 +8,7 @@ public class Quiz05_SeqNumberSum {
         int n = sc.nextInt();
         System.out.println(answer1(n));
         System.out.println(answer2(n));
+        System.out.println(solution1(n));
     }
 
     private static int answer1(int n) {
@@ -33,6 +34,31 @@ public class Quiz05_SeqNumberSum {
             while (sum >= n) {
                 sum -= lp++;
                 if (sum == n) answer++;
+            }
+        }
+        return answer;
+    }
+
+    /**
+     * 두 수중 가능한지 확인하기 위해서는 기준값 15에서
+     * 첫번째 숫자로 1을 할당
+     * 두번째 숫자로 2를 할당한다.
+     * 연속된 두 수
+     * @param n
+     * @return
+     */
+    private static int solution1(int n) {
+        int answer=0, sum=0;
+        int m=n/2+1;
+        int[] arr=new int[m];
+        for(int i=0; i<m; i++) arr[i]=i+1;
+        int lt=0;
+        for(int rt=0; rt<m; rt++){
+            sum+=arr[rt];
+            if(sum==n) answer++;
+            while(sum>=n){
+                sum-=arr[lt++];
+                if(sum==n) answer++;
             }
         }
         return answer;
