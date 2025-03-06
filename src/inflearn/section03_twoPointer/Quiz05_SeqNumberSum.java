b1dd35c67f2c34c18b2cc349c5ca69a26ae566e4
webdevyoo <yjk7454@naver.com>
2025-03-07 02:08:23 +0900

    인프런 Section03_투포인터,슬라이딩윈도우 - 05) `연속된 자연수의 합` 일반 풀이2

--- src/inflearn/section03_twoPointer/Quiz05_SeqNumberSum.java
+++ src/inflearn/section03_twoPointer/Quiz05_SeqNumberSum.java
@@ -7,8 +7,8 @@ public class Quiz05_SeqNumberSum {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         System.out.println(answer1(n));
+        System.out.println(answer2(n));
     }
-
 
     private static int answer1(int n) {
         int answer = 0, sum = 1, pl = 1, pr = 1;
@@ -20,6 +20,19 @@ public class Quiz05_SeqNumberSum {
                 ++answer;
                 sum += ++pr;
                 sum -= pl++;
+            }
+        }
+        return answer;
+    }
+    // 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
+    private static int answer2(int n) {
+        int sum =0, lp =1, answer =0;
+        for (int rp = 1; rp < n; rp++) {
+            sum += rp;
+            if (sum == n) answer ++;
+            while (sum >= n) {
+                sum -= lp++;
+                if (sum == n) answer++;
             }
         }
         return answer;
