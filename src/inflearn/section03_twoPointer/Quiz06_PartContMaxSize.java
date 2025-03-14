415601c0c5ee774d7444fa2b51de1470ed373523
webdevyoo <yjk7454@naver.com>
2025-03-15 01:23:52 +0900

    인프런 Section03_투포인터,슬라이딩윈도우 - 06) `최대 길이 연속부분수열` 해설풀이

--- src/inflearn/section03_twoPointer/Quiz06_PartContMaxSize.java
+++ src/inflearn/section03_twoPointer/Quiz06_PartContMaxSize.java
@@ -13,8 +13,10 @@ public class Quiz06_PartContMaxSize {
         }
 
         System.out.println(wrong_answer(n, k, sales));
+        System.out.println(solution(n, k, sales));
     }
-    
+
+
     /**
      *
      * @param n
@@ -43,5 +45,26 @@ public class Quiz06_PartContMaxSize {
         }
         return answer;
     }
+
+    /**
+     * 우측 포인터가 가리키는곳이 0 이면 해당 지점을 1로 변경하고 count값을 1 증가시킨다.
+     * count값이 k보다 커진다면, 변경한 1지점을 다시 0으로 변경한다.
+     * 좌측~우측 까지의 길이 : 우측포인터 - 좌측포인터 + 1
+     * count값이 k보다 커지면 변경했던 1을 다시 0으로 로 변경한다고 했으나, 실제로 변경하지 않고 count만 샌다.
+     * 즉, count가 k값을 초과하지 않을때까지 pl값을 증가시켜 가며, count값을 감소시킨다.
+     * 이후 pl~pr의 길이를 구할때 pl값은 1에서 0으로 변경한 인덱스의 다음인덱스부터 길이를 구해야한다.
+     * 위 과정은 1을 0으로 변경하고 해당 index 다음 값으로 pl값을 이동하는것과 같다.
+     */
+    private static int solution(int n, int k, int[] sales) {
+        int count = 0, answer = 0, pl = 0;
+        for (int pr = 0; pr < n; pr++) {
+            if (sales[pr] == 0) count ++; // 0이면 1씩 카운트
+            while(count > k) if(sales[pl] == 0) count--;
+            if (answer < pr - pl + 1) answer = pr - pl + 1; // pl조정후 max값 초기화
+        }
+
+        return answer;
+    }
+
 
 }
