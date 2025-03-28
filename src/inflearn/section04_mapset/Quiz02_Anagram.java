c9adea57a0e126fb18298a3e29446152c38a3d89
webdevyoo <yjk7454@naver.com>
2025-03-29 01:06:49 +0900

    인프런 Section04_해시맵- 01) `아나그램(해쉬)` 일반 풀이2

--- src/inflearn/section04_mapset/Quiz02_Anagram.java
+++ src/inflearn/section04_mapset/Quiz02_Anagram.java
@@ -10,6 +10,7 @@ public class Quiz02_Anagram {
         String str1 = sc.next();
         String str2 = sc.next();
         System.out.println(answer1(str1, str2));
+        System.out.println(answer2(str1, str2));
     }
 
     private static String answer1(String str1, String str2) {
@@ -18,6 +19,35 @@ public class Quiz02_Anagram {
         Arrays.sort(chars1);
         Arrays.sort(chars2);
         return Arrays.equals(chars1, chars2) ? "YES" : "NO" ;
+    }
+
+    /**
+     * getOrDefault를 1로 해야하는 이유
+     * getOrDefault가 0이면
+     * aabbb(2,3) / aaabb일때, 2-3 = -1 / 3-2 = 1
+     * 즉 -1 + 1 = 0이 되기 때문에 누적합이 0이되어 모순이 생긴다.
+     * 비교해야할 대상 값보다 비교값이 1개 더 많으면 무조건 음수가 나오기 때문이다.
+     * 하지만 1로 처리를 한다면
+     * aabbb(3,4) / aaabb 일때, 3-3 = 0 / 4-2 = 2
+     * 즉 0 + 2 = 2가 되기 때문에 누적합이 0이 아니므로 "NO"가 된다.
+     */
+    private static String answer2(String str1, String str2) {
+        char[] chars = str1.toCharArray();
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < chars.length; i++) {
+            map.put(chars[i], map.getOrDefault(chars[i], 1) + 1);
+        }
+        char[] chars2 = str2.toCharArray();
+        for (int i = 0; i < chars2.length; i++) {
+            if (map.containsKey(chars2[i])) {
+                map.put(chars2[i], map.get(chars2[i]) - 1);
+            }
+        }
+        int sum = 0;
+        for (Character character : map.keySet()) {
+            sum += map.get(character);
+        }
+        return sum == 0 ? "YES" : "NO";
     }
 
 }
