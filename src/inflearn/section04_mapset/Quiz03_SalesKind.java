dbff325b8fd9d913b6a801c7f7f43a451cb3ebe3
webdevyoo <yjk7454@naver.com>
2025-03-30 17:38:39 +0900

    인프런 Section04_해시맵- 03) `매출액의 종류` 일반 풀이

---
+++ src/inflearn/section04_mapset/Quiz03_SalesKind.java
@@ -0,0 +1,44 @@
+package inflearn.section04_mapset;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Quiz03_SalesKind {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i : answer(n, k, arr)) {
+            System.out.print(i + " ");
+        }
+
+    }
+
+    private static ArrayList<Integer> answer(int n, int k, int[] arr){
+        ArrayList<Integer> answer = new ArrayList<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < k; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+        answer.add(map.size());
+        int lp = 0, rp = k-1;
+        while (lp < n-k || rp < n ) {
+            if(lp == n-k || rp == n) break;
+            if (map.get(arr[lp]) == 1) {
+                map.remove(arr[lp++]);
+            } else {
+                map.put(arr[lp], map.get(arr[lp++]) - 1);
+            }
+            map.put(arr[++rp], map.getOrDefault(arr[rp], 0) + 1);
+            answer.add(map.size());
+        }
+        return answer;
+    }
+
+}
