e532a1d992f3dabcf76c59bab132d0e92b9dfeba
webdevyoo <yjk7454@naver.com>
2025-03-30 17:38:54 +0900

    인프런 Section04_해시맵- 03) `매출액의 종류` 해설 풀이

--- src/inflearn/section04_mapset/Quiz03_SalesKind.java
+++ src/inflearn/section04_mapset/Quiz03_SalesKind.java
@@ -17,7 +17,9 @@ public class Quiz03_SalesKind {
         for (int i : answer(n, k, arr)) {
             System.out.print(i + " ");
         }
-
+        for (int i : solution(n, k, arr)) {
+            System.out.print(i + " ");
+        }
     }
 
     private static ArrayList<Integer> answer(int n, int k, int[] arr){
@@ -37,6 +39,23 @@ public class Quiz03_SalesKind {
             }
             map.put(arr[++rp], map.getOrDefault(arr[rp], 0) + 1);
             answer.add(map.size());
+        }
+        return answer;
+    }
+
+    private static  ArrayList<Integer> solution(int n, int k, int[] arr){
+        ArrayList<Integer> answer = new ArrayList<>();
+        HashMap<Integer, Integer> HM = new HashMap<>();
+        for(int i=0; i<k-1; i++){
+            HM.put(arr[i], HM.getOrDefault(arr[i], 0)+1);
+        }
+        int lt=0;
+        for(int rt=k-1; rt<n; rt++){
+            HM.put(arr[rt], HM.getOrDefault(arr[rt], 0)+1);
+            answer.add(HM.size());
+            HM.put(arr[lt], HM.get(arr[lt])-1);
+            if(HM.get(arr[lt])==0) HM.remove(arr[lt]);
+            lt++;
         }
         return answer;
     }
