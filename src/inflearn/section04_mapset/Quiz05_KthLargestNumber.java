3ef2a8d70db28d79dc67250b66c4eece0e5b133e
YooHyeok <yjk7454@naver.com>
2025-04-26 14:03:24 +0900

    인프런 Section04_해시맵- 04) `모든 아나그램 찾기` 해설 풀이

--- src/inflearn/section04_mapset/Quiz05_KthLargestNumber.java
+++ src/inflearn/section04_mapset/Quiz05_KthLargestNumber.java
@@ -12,6 +12,7 @@ public class Quiz05_KthLargestNumber {
             ints[i] = sc.nextInt();
         }
         System.out.println(answer(n, k, ints));
+        System.out.println(solution(n, k, ints));
     }
 
     /**
@@ -33,6 +34,26 @@ public class Quiz05_KthLargestNumber {
             }
         }
         return treeSet.toArray().length < k ? -1 : (int)treeSet.toArray()[treeSet.toArray().length-k];
+    }
+
+    /**
+     * i는 0부터 j는 i+1부터 l은 j+1부터 시작하여 루프를 돌리게 되면 중복이 제거된다.
+     */
+    private static int solution(int n, int k, int[] ints) {
+        TreeSet<Integer> treeSet = new TreeSet<>(Collections.reverseOrder());
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                for (int l = j+1; l < n; l++) {
+                    treeSet.add(ints[i] + ints[j] + ints[l]);
+                }
+            }
+        }
+        int count = 0;
+        for (int num : treeSet) {
+            count++;
+            if (count == k) return num;
+        }
+        return -1;
     }
 
 }
