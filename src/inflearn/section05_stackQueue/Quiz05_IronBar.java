71519d453de361bb4f22060e378c993728f0178f
YooHyeok <yjk7454@naver.com>
2026-01-18 00:14:15 +0900

    인프런 Section05_스택큐- 04) `쇠막대기` 해설 풀이

--- src/inflearn/section05_stackQueue/Quiz05_IronBar.java
+++ src/inflearn/section05_stackQueue/Quiz05_IronBar.java
@@ -8,6 +8,7 @@ public class Quiz05_IronBar {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
         System.out.println(answer(str));
+        System.out.println(solution(str));
     }
     private static int answer(String str) {;
         int answer = 0;
@@ -28,4 +29,25 @@ public class Quiz05_IronBar {
         return answer;
     }
 
+    /**
+     * 첫 시작 괄호가 무조건 열린다는 전제로 짠 코드.
+     * 현재 괄호가 열린 상태라면 stack에 열린괄호를 추가.
+     * 현재 괄호가 닫힌 상태라면 stack에서 최신으로 삽입된 열린괄호를 제거한 뒤
+     * (닫힌 상태라면 stack이 무조건 비어있지 않은 경우)
+     *  만약 이전 문자가 열려있었다면 현재까지 쌓여있는 stack 즉, 열린 괄호의 갯수를 누적으로 저장하고,
+     *  아니라면 (이전 문자가 닫혀있었다면) 값을 1 증가시킨다.
+     */
+    private static int solution(String str) {
+        int answer = 0;
+        Stack<Character> stack = new Stack();
+        for (int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == '(') stack.push('(');
+            else {
+                stack.pop();
+                if (str.charAt(i-1) == '(') answer += stack.size();
+                else answer ++;
+            }
+        }
+        return answer;
+    }
 }
